ff768ed3b44e05eadfcdb0084b30b012fd47a8e8
rabeebkm <132348148+rabeebkm@users.noreply.github.com>
2023-05-08 11:30:34 +0530

    Create intro.py

---
+++ Python/intro.py
@@ -0,0 +1,6 @@
+name="Rabeeb K M"
+age=18
+clg="GPTC Chelakkara, Thrissur"
+dept="Computer Engineering"
+place="Thrissur"
+print("Self intro\n""My name is", name,". \nI'm",age, ". \nI'm coming from", place, ". \nI'm Studying at", clg,"\nas a", dept, "Student .")
